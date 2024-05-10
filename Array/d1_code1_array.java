92b2d2d2f026007633d08d44819e697c8f51792e
chaudharysurya14 <114549294+chaudharysurya14@users.noreply.github.com>
2024-05-10 11:22:59 +0530

    update Array 1-d code

---
+++ Array/d1_code1_array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class d1_code1_array {
+    public static void main(String args[]) {
+        int a[] = { 10, 20, 30, 40, 50 }; // a[5]
+        for (int b : a) {
+            System.out.println(b + " ");
+        }
+    }
+}
